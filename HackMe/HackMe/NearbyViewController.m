fe24c47afcab5bc136b5103f4502b6d4d2a69611
Tamim ziad <ziadtdi@gmail.com>
2012-09-30 18:04:06 +0000

    NearBy

--- HackMe/HackMe/NearbyViewController.m
+++ HackMe/HackMe/NearbyViewController.m
@@ -8,8 +8,20 @@
 
 #import "NearbyViewController.h"
 
-@interface NearbyViewController ()
+#import "UIImageView+Network.h"
+#import "Video.h"
+#import "User.h"
+#import "VideoBuilder.h"
+#import "HackMeManager.h"
+#import "HackMeCommunicator.h"
+#import "HackMeManager.h"
+#import "DisplayVideoViewController.h"
 
+@interface NearbyViewController ()<HackMeManagerDelegate>
+@property (strong) HackMeManager *manager;
+@property (strong) NSArray *videos;
+
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath;
 @end
 
 @implementation NearbyViewController
@@ -17,13 +29,86 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    self.tableView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"background.png"]];
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    
+    _manager = [[HackMeManager alloc] init];
+    self.manager.communicator = [[HackMeCommunicator alloc] init];
+    self.manager.communicator.delegate = _manager;
+    self.manager.videoBuilder = [[VideoBuilder alloc] init];
+    self.manager.delegate = self;
+    
+    [self.manager fetchFeedFromYoutube];
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - Manager Delegate
+
+- (void)fetchVideoFromYoutubeFailedWithError:(NSError *)error
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+}
+
+- (void)didReceiveVideo:(NSArray *)videos
+{
+    self.videos = [videos copy];
+    [self.tableView reloadData];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.videos count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    [self configureCell:cell atIndexPath:indexPath];
+    
+    return cell;
+}
+
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath
+{
+    Video *video = [self.videos objectAtIndex:indexPath.row];
+    
+    [[cell.contentView viewWithTag:1] removeFromSuperview];
+    UIImageView *avatarVideo = [[UIImageView alloc] initWithFrame:CGRectMake(27, 17, 267, 139)];
+    [avatarVideo setTag:1];
+    
+    [avatarVideo loadImageWithUrl:video.avatarUrl];
+    [cell.contentView addSubview:avatarVideo];
+    
+    UILabel *titleLabel = (UILabel *)[cell.contentView viewWithTag:3];
+    [titleLabel setText:video.title];
+    
+    [[cell.contentView viewWithTag:2] removeFromSuperview];
+    UIImageView *avatarUser = [[UIImageView alloc] initWithFrame:CGRectMake(27, 162, 31, 28)];
+    [avatarUser setTag:2];
+    
+    [avatarUser loadImageWithUrl:video.uploader.avatarUrl];
+    [cell.contentView addSubview:avatarUser];
+    
+    UIImageView *playIcon = [[UIImageView alloc] initWithFrame:CGRectMake(131, 66, 55, 41)];
+    [playIcon setImage:[UIImage imageNamed:@"play-big.png"]];
+    [cell.contentView addSubview:playIcon];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier] isEqualToString:@"ShowVideo"]) {
+        Video *video = [self.videos objectAtIndex:[[self.tableView indexPathForSelectedRow] row]];
+        [[segue destinationViewController] setVideo:video];
+    }
 }
 
 @end
